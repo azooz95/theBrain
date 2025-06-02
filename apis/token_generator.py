dccaf007aa56853939543edb9fc442df60efe9a0
azooz95 <aziz.alhaj30@gmail.com>
2025-06-02 13:31:34 +0300

    adding websocket

--- apis/token_generator.py
+++ apis/token_generator.py
@@ -15,12 +15,14 @@ FAKE_USER = {
 }
 
 ALGORITHM = "HS256"
-DEFAULT_EXPIRATION_HOURS = 24
+DEFAULT_EXPIRATION_HOURS = 24 * 365 # 1 year
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def generate_token(user_id, expiration_hours=DEFAULT_EXPIRATION_HOURS):
+def generate_token(user_data, expiration_hours=DEFAULT_EXPIRATION_HOURS):
     payload = {
-        'user_id': user_id,
+        'user_id': user_data["user_id"],
+        'username': user_data["username"],
+        'email': user_data["email"],
         'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=expiration_hours)
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
@@ -41,17 +43,28 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
             headers={"WWW-Authenticate": "Bearer"},
         )
 
+def check_token(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
 @app.get("/users")
 def read_users_me(current_user: str = Depends(get_current_user)):
     return {"user_id": current_user, "email": FAKE_USER["email"]}
 
 if __name__ == "__main__":
     user_id = "1"  # Example
-    token = generate_token(user_id)
+    token = generate_token(FAKE_USER)
     print(f"Generated JWT Token for user {user_id}: {token}")
 
     # decode_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
     # print(f"Decoded Token: {decode_token}")
 
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    # import uvicorn
+    # uvicorn.run(app, host="localhost", port=8000)
