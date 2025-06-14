01b6bc3897d0904ca577f8a51155227c76ee39b2
joe13thadmin <services@joe13th.com>
2025-06-14 19:37:51 +0400

    Update main.py

--- apis/main.py
+++ apis/main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from apis.routers.chat import router as chat_router
